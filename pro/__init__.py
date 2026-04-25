52595352146d7b6b2b096d81df563b39e3ce3413
aitispec <aitispec@ya.ru>
2026-04-25 13:52:36 +0300

    add .gitignore add install.ps1 add run.bat update readme

--- pro/__init__.py
+++ pro/__init__.py
@@ -1,1 +1,4 @@
-
+from .model_manager import ModelManager
+from .presets import load_presets_from_files
+from .ui_components import STYLES, COPY_BUTTON_JS, COPY_BUTTON_HTML
+from .formatters import format_response_display
